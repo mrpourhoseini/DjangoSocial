e0822f5d12abd7e514c62f0506113415aa71288e
Mohammadreza Pourhoseini <mohammadreza.pour.computer@gmail.com>
2023-10-01 23:42:58 +0330

    Created post list view

--- apps/social/views.py
+++ apps/social/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import View
+
+from apps.social.models import Post
+
 
 # Create your views here.
+class PostListView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all().order_by('-created_at')
+
+        context = {
+            'post_list': posts,
+        }
+
+        return render(request, 'social/post_list.html', context)
